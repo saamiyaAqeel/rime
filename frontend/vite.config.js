d5762c67a6a656592f4ba335c849a68c62b68d4c
Nicholas FitzRoy-Dale <nicholas@telemarq.com>
2023-06-07 13:01:13 +0100

    explicitly use 'localhost' for front- and backend
    Without being explicit, flask preferentially binds to 127.0.0.1 and vite
    binds to localhost; these are different if ipv6 is supported.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [vue()],
     server : {
+        host: 'localhost',
         port: 3000,
         strictPort: true,
     },
