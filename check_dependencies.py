bf36f8020b4ee92a64ad7314631fc536e08926a1
Nicholas FitzRoy-Dale <nicholas@telemarq.com>
2023-08-31 14:00:17 +0100

    fix check_dependencies for editable packages

--- check_dependencies.py
+++ check_dependencies.py
@@ -18,14 +18,34 @@ current_packages: Dict[str, str] = {}
 for dist in pkg_resources.working_set:
     current_packages[dist.project_name] = dist.version
 
+def check_package(req: str) -> bool:
+    " Does 'req' (requirements.txt line) match the currently installed packages? "
+    # Get the package name and the version
+    version = None  # shut linter up
+    if req.startswith('-e'):
+        # Editable. Just check package name without version.
+        if 'egg=' in req:
+            project_name = req.split('#egg=')[1]
+            check_version = False
+        else:
+            project_name = req.split('/')[-1]
+            check_version = False
+    else:
+        project_name, version = req.strip().split('==')
+        check_version = True
+
+    project_name = project_name.replace('_', '-')
+
+    if project_name not in current_packages:
+        return False
+
+    if check_version and current_packages[project_name] != version:
+        return False
+
+    return True
+
 with open('requirements.txt') as requirements:
     for req in requirements:
-        # Get the package name and the version
-        project_name, version = req.strip().split('==')
-        project_name = project_name.replace('_', '-')
-
-        # If the package is not installed or there is a version mismatch
-        # exit with code (1)
-        if (project_name not in current_packages or current_packages[project_name] != version):
-            print(f'Requirements do not match for {project_name}')
+        if not check_package(req.strip()):
+            print(f'Requirements do not match for {req}')
             sys.exit(1)
