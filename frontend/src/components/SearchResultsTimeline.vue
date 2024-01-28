a749a49e342a11df905ea23c41fca708c219d574
psysa11 <psysa11@exmail.nottingham.ac.uk>
2024-01-28 17:14:28 +0000

    feat: add colours to the timeline according to their title

--- frontend/src/components/SearchResultsTimeline.vue
+++ frontend/src/components/SearchResultsTimeline.vue
@@ -2,11 +2,12 @@
 <template>
   <div class="timeline-container">
     <div :id="containerId"></div>
+    <div id="legendContainer"></div>
   </div>
 </template>
 
 <script>
-import * as anychart from 'anychart'
+import * as anychart from 'anychart';
 
 export default {
   data() {
@@ -21,15 +22,21 @@ export default {
           '/timeline.json',
           (data) => {
             var chart = anychart.timeline();
+            var series;
 
             for (var i = 0; i < data.Timeline.length; i++) {
-              var series = chart.range([
+              series = chart.range([
                 [
                   data.Timeline[i].title,
                   data.Timeline[i].start,
                   data.Timeline[i].end,
                 ],
               ]);
+
+              // Customize the series if needed
+              series.normal().fill(this.getColorByTitle(data.Timeline[i].title));
+              series.hovered().fill(this.getColorByTitle(data.Timeline[i].title));
+              series.selected().fill(this.getColorByTitle(data.Timeline[i].title));
             }
 
             var pfizerDataSet = anychart.data.set(data.ImportantEvents);
@@ -60,31 +67,67 @@ export default {
             chart.axis().labels().format(function () {
               return anychart.format.dateTime(this.tickValue, 'MMM yyyy');
             });
-            
+
             chart.scroller().enabled(true);
             chart.title('Timeline of Events');
             chart.container(this.containerId);
+
             chart.draw();
+
+            this.createLegend(data.Timeline, chart);
           }
         );
       });
     });
   },
+  methods: {
+    createLegend(timelineData, chart) {
+      // Get the legend container
+      var legendContainer = document.getElementById('legendContainer');
+
+      // Iterate over the timeline data to create legend items
+      for (var i = 0; i < timelineData.length; i++) {
+        var legendItem = document.createElement('div');
+        legendItem.innerHTML = timelineData[i].title;
+        legendItem.style.color = chart.getSeriesAt(i).color;
+
+        // Append the legend item to the legend container
+        legendContainer.appendChild(legendItem);
+      }
+    },
+    getColorByTitle(title) {
+      if (title.includes("A")) {
+        return "#ff0000"; // Red for titles containing "A"
+      } else if (title.includes("B")) {
+        return "#00ff00"; // Green for titles containing "B"
+      } else if (title.includes("C")) {
+        return "#0000ff"; // Blue for titles containing "C"
+      } else {
+        return "#808080"; // Default color for other titles
+      }
+    }
+  },
 };
 </script>
+
 
 <style scoped>
 .timeline-container {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  justify-content: center;
   height: 100vh;
 }
 
 #container {
   width: 90%;
-  height: 90%;
+  height: 60%;
   margin: 0;
   padding: 0;
+}
+
+#legendContainer {
+  margin-top: 20px;
 }
 </style>
